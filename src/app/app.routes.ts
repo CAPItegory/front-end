af91d1b1e5854c59b41c8557aca9e4a147e81bc8
Axelle MAZUY <axelle.mazuy@gmail.com>
2024-10-15 15:24:13 +0200

    feat: initialization of category and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', component: HomeComponent },
+    { path : 'category', component: CategoryComponent }
+];
