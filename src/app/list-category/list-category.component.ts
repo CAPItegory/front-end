e9cfaa6084f203620b0472aed2701485281d0f44
ellexart <axelle.mazuy@gmail.com>
2024-11-01 14:03:40 +0100

    feat: add integration of remaining endpoint in capitegory.service with backend

--- src/app/list-category/list-category.component.ts
+++ src/app/list-category/list-category.component.ts
@@ -13,8 +13,23 @@ export class ListCategoryComponent {
   constructor(private capitegoryService: CapitegoryService) {}
 
   async ngOnInit() {
-    let result = await this.capitegoryService.getById("0d24129f-706f-453d-348d-08dcf6893c4a")
+    let result2 = await this.capitegoryService.getAll()
+    //console.log(result2)
+
+    //let result3 = await this.capitegoryService.create("JE SUIS UN CAPYBARA BEBE", '8ff916d4-55e6-4f95-4f94-08dcfa69437b')
+    //console.log(result3)
+
+    let result4 = await this.capitegoryService.update("6da314c4-43af-460c-4f9e-08dcfa69437b", "JE SUIS UN SUPER MIGNON CAPYBARA BEBE", "8dd29358-9c2c-4b1c-4f98-08dcfa69437b")
+    console.log(result4)
+
+    let result = await this.capitegoryService.getById("6da314c4-43af-460c-4f9e-08dcfa69437b")
     console.log(result)
+
+    let result5 = await this.capitegoryService.delete("6da314c4-43af-460c-4f9e-08dcfa69437b");
+    console.log(result5)
+
+    let result6 = await this.capitegoryService.search();
+    console.log(result6)
   }
 
 }
--- src/app/service/capitegory.service.ts
+++ src/app/service/capitegory.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from "@angular/core";
 import { Category } from "../entity/category.entity";
+import { METHODS } from "node:http";
 
 @Injectable({
     providedIn: "root"
 })
 export class CapitegoryService 
 {
-    private apiUrl : string = "http://localhost:4200/api/category"; //TODO : env variable
+    private apiUrl : string = "http://localhost:4200/api/category/"; //TODO : env variable
 
-    public async getById(id: string) : Promise<Category | null>
+    public async getAll() : Promise<Category[]>
     {
-        return await fetch(this.apiUrl + "/" + id)
+        return await fetch(this.apiUrl)
+            .then(res => res.ok ? res.json() : null)
+            .then(res => {
+                if (res == null) {
+                    return null;
+                }
+                return res
+            })
+    }
+
+    public async create(name: string, parentId: string|null = null) : Promise<Category | null>
+    {
+        return await fetch(this.apiUrl, { 
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name: name, parent: parentId}),
+        })
             .then(res => res.ok ? res.json() : null)
             .then(res => {
                 if (res == null) {
@@ -19,4 +36,75 @@ export class CapitegoryService
                 return new Category(res.id, res.name, res.creationDate, res.isRoot, res.children)
             })
     }
+
+    public async getById(id: string) : Promise<Category | null>
+    {
+        return await fetch(this.apiUrl + id)
+            .then(res => res.ok ? res.json() : null)
+            .then(res => {
+                if (res == null) {
+                    return null;
+                }
+                return new Category(res.id, res.name, res.creationDate, res.isRoot, res.children)
+            })
+    }
+
+    public async update(id: string, name: string|null = null, parentId: string|null = null)
+    {
+        await fetch(this.apiUrl + id, { 
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name: name, parent: parentId}),
+        })
+    }
+
+    public async delete(id: string)
+    {
+        await fetch(this.apiUrl + id, { 
+            method: "DELETE",
+        })
+    }
+
+    public async search(isRoot: boolean|null = null, beforeDate: Date|null = null, afterDate: Date|null = null, parentId: string|null = null, 
+        orderByName: boolean|null = null, orderbyCreationDate: boolean|null = null, orderByNumberOfChildren: boolean|null = null, 
+        pageNumber: number|null = null, pageSize: number|null = null) : Promise<Category[]>
+    {
+        let params = new URLSearchParams()
+        if (isRoot != null) {
+            params.append("IsRoot", isRoot.valueOf.toString())
+        }
+        if (beforeDate != null) {
+            params.append("BeforeDate", beforeDate.valueOf.toString())
+        }
+        if (afterDate != null) {
+            params.append("AfterDate", afterDate.valueOf.toString())
+        }
+        if (parentId != null) {
+            params.append("ParentId", parentId.valueOf.toString())
+        }
+        if (orderByName != null) {
+            params.append("OrderByName", orderByName.valueOf.toString())
+        }
+        if (orderbyCreationDate != null) {
+            params.append("OrderByCreationDate", orderbyCreationDate.valueOf.toString())
+        }
+        if (orderByNumberOfChildren != null) {
+            params.append("OrderByNumberOfChildren", orderByNumberOfChildren.valueOf.toString())
+        }
+        if (pageNumber != null) {
+            params.append("PageNumber", pageNumber.valueOf.toString())
+        }
+        if (pageSize != null) {
+            params.append("PageSize", pageSize.valueOf.toString())
+        }
+        return await fetch(this.apiUrl + "search?" + params)
+            .then(res => res.ok ? res.json() : null)
+            .then(res => {
+                if (res == null) {
+                    return null;
+                }
+                return res
+            })
+    }
+
 }
